2978cf22f5ed01cb1d4d4f7ce51d05f38baccc86
Michael Griffith <griffithcmichael@gmail.com>
2020-11-09 15:00:07 -0600

    start over

--- javaScript.js
+++ javaScript.js
@@ -1,24 +1,4 @@
+
+
+
 let APIKey = "2b0e45595d4ae6038fa0a7f4e34573f0";
-
-// Here we are building the URL we need to query the database
-
-$("city-input").submit(function(event){
-    event.preventDefault();
-
-    let cityName = $("city-name").val();
-    searchWeather(cityName);
-});
-
-function searchWeather(city){
-let queryURL = "http://api.openweathermap.org/data/2.5/forcast?q="+city+"id=524901&appid=2b0e45595d4ae6038fa0a7f4e34573f0";
-    
-
-
-
-// We then created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response)
-})};searchWeather();

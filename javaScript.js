2758f2fac2afa373e28c0253da6093ed7e0296b4
Michael Griffith <griffithcmichael@gmail.com>
2020-11-09 15:43:14 -0600

    start search function

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,11 @@
 
-
+let searchButton = $(".searchButton").on("click", searchFunction);
 
 let APIKey = "2b0e45595d4ae6038fa0a7f4e34573f0";
+
+function searchFunction (){
+  localStorage.setItme("inputcontent-" + searchInput.val(), searchInput.val());
+  localStorage.setItme("lastresult", searchInput.val());
+
+}
+
